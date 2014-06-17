8924434d473aed859befa0ae0bdf6c01caae2195
蓝牙F <547608958@qq.com>
2014-06-17 16:06:17 +0800

    基础结构搭建

---
+++ geekplay/assets/bower_components/seajs-config.js
@@ -0,0 +1,24 @@
+seajs.config({
+	base:'/bower_components/',
+	alias:{
+		'jquery':'jquery/dist/jquery.min.js',		
+		"bootstrap": "bootstrap/dist/js/bootstrap.min.js",	
+	},
+	// 预加载项
+	preload: [
+	this.JSON ? '' : 'json'],
+
+	// 路径配置
+	paths: {
+		'common': 'common/'
+	},
+
+	// 变量配置
+	vars: {
+		'locale': 'zh-cn'
+	},
+
+	charset: 'utf-8',
+
+	debug: true
+});
--- geekplay/assets/js/app.js
+++ geekplay/assets/js/app.js
@@ -1,71 +1,51 @@
-/**
- * app.js
- *
- * This file contains some conventional defaults for working with Socket.io + Sails.
- * It is designed to get you up and running fast, but is by no means anything special.
- *
- * Feel free to change none, some, or ALL of this file to fit your needs!
- */
+define(function(require, exports, module) {
+  window.$ = window.jQuery = require('jquery');
+  require('bootstrap');
 
-
-(function (io) {
-
-  // as soon as this file is loaded, connect automatically, 
-  var socket = io.connect();
-  if (typeof console !== 'undefined') {
-    log('Connecting to Sails.js...');
-  }
-
-  socket.on('connect', function socketConnected() {
-
-    // Listen for Comet messages from Sails
-    socket.on('message', function messageReceived(message) {
-
-      ///////////////////////////////////////////////////////////
-      // Replace the following with your own custom logic
-      // to run when a new message arrives from the Sails.js
-      // server.
-      ///////////////////////////////////////////////////////////
-      log('New comet message received :: ', message);
-      //////////////////////////////////////////////////////
-
+  exports.load = function(name) {
+    require.async('./' + name + '.js', function(controller){
+      if ($.isFunction(controller.run)) {
+        controller.run();
+      }
     });
+  };
 
+/*var app = angular.module('app', ['ngSea']);
 
-    ///////////////////////////////////////////////////////////
-    // Here's where you'll want to add any custom logic for
-    // when the browser establishes its socket connection to 
-    // the Sails.js server.
-    ///////////////////////////////////////////////////////////
-    log(
-        'Socket is now connected and globally accessible as `socket`.\n' + 
-        'e.g. to send a GET request to Sails, try \n' + 
-        '`socket.get("/", function (response) ' +
-        '{ console.log(response); })`'
-    );
-    ///////////////////////////////////////////////////////////
+    app.config(['$routeProvider', "$controllerProvider", function ($routeProvider, $controllerProvider) {
+        $routeProvider
+            .when('/t1', {
+                controller: 'testACtrl',
+                templateUrl: './m1/t1.html',
+                // 'controllerUrl': 'm1/t1'
+            })
+            .when('/t2',{
+                controller: 'testBCtrl',
+                templateUrl: '/home/contact',
+                // templateUrl: './m2/t2.html',
+                // 'controllerUrl': 'm2/t2'
+            })
+            
 
-
-  });
-
-
-  // Expose connected `socket` instance globally so that it's easy
-  // to experiment with from the browser console while prototyping.
-  window.socket = socket;
-
-
-  // Simple log function to keep the example simple
-  function log () {
-    if (typeof console !== 'undefined') {
-      console.log.apply(console, arguments);
+            .when('/', {
+                controller: function ($scope, $routeParams, $location) {
+                    $scope.str = new Date()
+                },
+                template: '<div>{{str}}</div>'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
     }
-  }
-  
+    ]);
 
-})(
+    app.run(["$rootScope", "$ngSea", function ($rootScope, $ngSea) {
 
-  // In case you're wrapping socket.io to prevent pollution of the global namespace,
-  // you can replace `window.io` with your own `io` here:
-  window.io
+        app = $ngSea(app);
 
-);
+    }]);*/
+
+    // module.exports = app;
+
+  window.load = exports.load;
+});
